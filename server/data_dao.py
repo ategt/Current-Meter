15f00cd9cebfa0c19ec75e5ffdabc585b5188906
Adam Tegtmeier <ategtmei@gmail.com>
2022-03-01 13:37:10 -0500

    Working on a data dao.  It is certainly in the wrong folder, but it has tests.  Some fail, but still, tests.

---
+++ server/data_dao.py
@@ -0,0 +1,31 @@
+import re
+import os
+
+
+class DataDao(object):
+    """docstring for DataDao"""
+    def __init__(self, dataPath):
+        super(DataDao, self).__init__()
+        self.dataPath = dataPath
+        self.DATA_LINE_REGEX = re.compile(r"(?P<timecode>[0-9\.]+)\,\t\s+Reading\: (?P<reading>[0-9\.\-]+)\t Adjusted: (?P<adjustedReading>[0-9\.\-]+)\t Amps: (?P<amps>[0-9\.\-]+)\t Watts: (?P<watts>[0-9\.\-]+)\,(?P<instant_temperature>[0-9]+)\,(?P<denoised_temperature_reading>[0-9]+)\,(?P<instant_exterior_temperature>[0-9]+)\,(?P<denoised_exterior_temperature_reading>[0-9]+)\,(?P<instant_frige_temperature>[0-9]+)\,(?P<denoised_frige_temperature_reading>[0-9]+)")
+
+    def listFiles(self):
+        return os.listdir(self.dataPath)
+
+    def readFile(self, fileName):
+        filePath = os.path.join(self.dataPath, fileName)
+
+        with open(filePath, 'r') as handle:
+            return handle.read()
+
+    def getData(self, fileName):
+        content = self.readFile(fileName)
+        return [self.DATA_LINE_REGEX.match(line).groupdict() for line in content.strip().split("\n")]
+
+    def getTimes(self, fileName, start, end):
+        if start > end:
+            raise Exception("Start time must occurr before end time!")
+
+        data = self.getData(fileName)
+
+        return [datum for datum in data if float(datum['timecode']) > start and float(datum['timecode']) < end]
---
+++ server/test_data_dao.py
@@ -0,0 +1,33 @@
+import unittest
+import os
+
+from .data_dao import DataDao
+
+class TestDataDao(unittest.TestCase):
+    def setUp(self):
+        os.environ['DATA_PATH'] = os.path.join("data")
+        self.dataDao = DataDao(os.environ['DATA_PATH'])
+
+    def tearDown(self):
+        pass
+
+    def test_listFiles(self):
+        files = self.dataDao.listFiles()
+        self.assertTrue(len(files) > 1)
+
+    def test_readFile(self):
+        files = self.dataDao.listFiles()
+        
+        for file in files:
+            content = self.dataDao.readFile(file)
+            self.assertTrue(len(content) > 1)
+
+    def test_getData(self):
+        files = self.dataDao.listFiles()
+        
+        for file in files:
+            data = self.dataDao.getData(file)
+            self.assertTrue(len(data) > 1)
+
+if __name__ == '__main__':
+    unittest.main()
